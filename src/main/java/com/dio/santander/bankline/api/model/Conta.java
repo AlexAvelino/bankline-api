960cb09c164f7c708dddf53a9fd2158d4f4b3ec6
Alex <alourenco08@gmail.com>
2022-05-11 20:56:54 -0300

    Versão final

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -5,10 +5,13 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class Conta {
+	
 	@Column(name = "conta_numero")
 	private Long numero;
+	
 	@Column(name = "conta_saldo")
 	private Double saldo;
+	
 	
 	public Long getNumero() {
 		return numero;
